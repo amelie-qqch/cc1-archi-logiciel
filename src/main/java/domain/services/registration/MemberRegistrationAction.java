bf105f1138fa5fc1d313049815291a29c518547e
amelie-qqch <amedouillard@hotmail.fr>
2022-01-08 19:22:01 +0100

    ajout constructeur MemberRegistrationAction

--- src/main/java/domain/services/registration/MemberRegistrationAction.java
+++ src/main/java/domain/services/registration/MemberRegistrationAction.java
@@ -5,4 +5,11 @@ public class MemberRegistrationAction {
     public String firstname;
     public String lastname;
     public String password;
+
+    public MemberRegistrationAction(String email, String firstname, String lastname, String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+    }
 }
